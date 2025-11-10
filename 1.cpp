e46f77fb43229da9e3ef625f16ab2667dc8c9c44
Harsh Kataria <harsh.a.kataria@outlook.com>
2025-11-10 12:05:25 +0530

    Solution added for 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,4 +1,25 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
-int main()
-{ }
+bool isPrime(int n) {
+    if (n <= 1)
+        return false;  
+    for (int i = 2; i <= n / 2; i++) {
+        if (n % i == 0)
+            return false;  
+    }
+    return true;  // number is prime
+}
+
+int main() {
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (isPrime(num))
+        cout << num << " is a prime number.";
+    else
+        cout << num << " is not a prime number.";
+
+    return 0;
+}
